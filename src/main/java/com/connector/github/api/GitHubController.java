b737abc7a7cb60d341f6d0155074e0c9135cc923
EditedBoy <editedboy19@gmail.com>
2024-06-26 17:51:57 +0300

    Removed unneeded APIs

--- src/main/java/com/connector/github/api/GitHubController.java
+++ src/main/java/com/connector/github/api/GitHubController.java
@@ -2,12 +2,8 @@ package com.connector.github.api;
 
 import com.connector.github.dto.RepositoryData;
 import com.connector.github.service.GitHubService;
-import java.time.Duration;
-import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
@@ -25,23 +21,5 @@ public class GitHubController implements GitHubControllerApi {
       @PathVariable("username") String username, ServerWebExchange exchange) {
 
     return Mono.just(ResponseEntity.ok(gitHubService.getRepositories(username)));
-  }
-
-  @GetMapping(
-      value = "/api/v2/users/{username}/repositories",
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public Flux<RepositoryData> getRepositoriesV2(
-      @PathVariable("username") String username) {
-
-    return gitHubService.getRepositories(username);
-  }
-
-  @GetMapping(
-      value = "/api/v1/ping",
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public Flux<String> ping() {
-    return Flux.fromStream(IntStream.range(0, 10)
-            .mapToObj(i -> "Health ping -> %s".formatted(i + 1)))
-        .delayElements(Duration.ofMillis(1000));
   }
 }
